ef2d30b6e205af5f6c69febc57638f70c3d44020
Alexylvan <37299372+Alexylvan@users.noreply.github.com>
2018-07-31 20:45:11 -0300

    NOW WITH MOUTHS

--- oopacman/Pacman.java
+++ oopacman/Pacman.java
@@ -7,6 +7,10 @@ package oopacman;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
+import static oopacman.Actor.Status.IDLE;
 import static oopacman.Key.*;
 import static oopacman.OOPacman.mapObject;
 
@@ -16,6 +20,10 @@ import static oopacman.OOPacman.mapObject;
  */
 public class Pacman extends Actor {
 
+    private double arc_start = 0;//posicao inicial da parte superior da boca
+    private double arc_length = 360.0;//comprimento do arco inicial (começa como um circulo completo)
+    private String mouth = "OPENING";
+
     public Pacman(int x, int y) {
         super(x, y, "Pacman");
         setSize((int) Math.floor(OOPacman.gameAreaObject.getWidth() * 0.05)); //20:15
@@ -24,21 +32,62 @@ public class Pacman extends Actor {
 
     @Override
     public void render(GraphicsContext graphics) {
-        graphics.setFill(new Color(1.00, 1.00, 0.50, 1.0));
+        graphics.save();
+        graphics.transform(new Affine(new Rotate(gira(), getX() + getSize() / 2, getY() + getSize() / 2)));
+        graphics.setFill(Color.YELLOW);
         graphics.setStroke(new Color(0, 0, 0, 1));
-        graphics.fillOval(getX(), getY(), getSize(), getSize());
-        graphics.strokeOval(getX(), getY(), getSize(), getSize());
+        graphics.arc(getX(), getY(), this.getSize(), this.getSize(), this.arc_start, this.arc_length);
+        graphics.fillArc(getX(), getY(), this.getSize(), this.getSize(), this.arc_start, this.arc_length, ArcType.ROUND);
+        graphics.strokeArc(getX(), getY(), this.getSize(), this.getSize(), this.arc_start, this.arc_length, ArcType.ROUND);
+        graphics.restore();
     }
     String lastStatus = "";
 
     @Override
     public void update(GraphicsContext graphics) {
+        if (getStatus() != IDLE) {
+            movimentoBoca();//calcula a abertura da boca
+        }
         direcionar(); //Obtem comandos do jogador
         mover(); //Realiza o movimento
         checkPathCollision(); //Colidiu com Path?
         checkGhostCollision(); //Colidiu com Ghost?
     }
     
+    private void movimentoBoca() {
+        if ("OPENING".equals(mouth)) {
+            if (arc_start < 60) {
+                arc_start = arc_start + getSpeed()*2.5;
+                arc_length = arc_length - 2 * getSpeed()*2.5;
+            } else {
+                mouth = "CLOSING";
+            }
+        }
+        if ("CLOSING".equals(mouth)) {
+            if (arc_start > 0) {
+                arc_start = arc_start - getSpeed()*3;
+                arc_length = arc_length + 2 * getSpeed()*3;
+            } else {
+                mouth = "OPENING";
+            }
+        }
+    }
+
+    private double gira() {
+        switch (this.getDirection()) {
+            case UP:
+                return 270.0;
+            case DOWN:
+                return 90.0;
+            case RIGHT:
+                return 0.0;
+            case LEFT:
+                return 180.0;
+            default:
+                return 0.0;
+        }
+    }
+
     private void direcionar() {
         if (isPressed(UP)) {
             direcionar(UP, getSpeed());
@@ -61,11 +110,11 @@ public class Pacman extends Actor {
     private void checkGhostCollision() {
         for (GameObject g : OOPacman.entityObjectList) {
             if (g instanceof Ghost) {
-                int psize = getSize()-7; // Mais fácil de ser pego
+                int psize = getSize() - 7; // Mais fácil de ser pego
                 int gsize = ((Ghost) g).getSize() + 10;
                 int gleft = 10;
                 int[] pacman = new int[]{getX(), getX() + psize, getY(), getY() + psize};
-                int[] ghost = new int[]{((Ghost) g).getX()-gleft, ((Ghost) g).getX()-gleft + gsize, ((Ghost) g).getY()-gleft, ((Ghost) g).getY()-gleft + gsize};
+                int[] ghost = new int[]{((Ghost) g).getX() - gleft, ((Ghost) g).getX() - gleft + gsize, ((Ghost) g).getY() - gleft, ((Ghost) g).getY() - gleft + gsize};
                 if (pacman[0] >= ghost[0] && pacman[0] <= ghost[0 + 1]) {
                     if (pacman[0 + 1] >= ghost[0] && pacman[0 + 1] <= ghost[0 + 1]) {
                         if (pacman[2] >= ghost[2] && pacman[2] <= ghost[2 + 1]) {
